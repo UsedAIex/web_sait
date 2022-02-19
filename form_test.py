5d96512b9f2bb092479909101343bc4ae8afb797
UsedAlex <miaf2289@gmail.com>
2022-02-19 23:39:50 +0300

    Попытка перенаправления страницы

--- form_test.py
+++ form_test.py
@@ -1,19 +1,16 @@
 from flask import Flask, render_template, redirect
-from flask_wtf import FlaskForm
-from wtforms import SubmitField
-class LoginForm(FlaskForm):
-    submit = SubmitField('Войти')
 
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = 'fgmwebgip756b]PRQE*o)FPBGF*9wgovuY($%^*%i&*87RP5DOHpgw59tu3rgergfok&jy54whigbtrusgry3urfbhi'
 
-@app.route('/')
-def start():
-    form = LoginForm()
-    if form.validate_on_submit():
-        return redirect('/login')
-    return render_template('title_list.html', form=form)
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import DataRequired
+
+
+class LoginForm(FlaskForm):
+    submit = SubmitField('Войти')
 
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -23,9 +20,6 @@ def login():
         return redirect('/success')
     return render_template('form.html', title='Авторизация', form=form)
 
-@app.route('/success', methods=['GET', 'POST'])
-def ew():
-    pass
 
 
 if __name__ == '__main__':
