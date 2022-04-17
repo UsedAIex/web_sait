485eed980254436e81b879293cb6ed38a2cdb767
Starchi1 <91777726+Starchi1@users.noreply.github.com>
2022-04-17 19:51:57 +0300

    Delete user.py

--- form/user.py
+++
@@ -1,9 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import PasswordField, StringField, TextAreaField, SubmitField, EmailField
-from wtforms.validators import DataRequired
-
-
-class RegisterForm(FlaskForm):
-    name = StringField('Имя пользователя', validators=[DataRequired()])
-    password = PasswordField('Пароль', validators=[DataRequired()])
-    submit = SubmitField('Войти')
