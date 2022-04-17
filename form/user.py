eb6ed7ba52c5989134cbd5e491b3c5036c46989c
Starchi1 <91777726+Starchi1@users.noreply.github.com>
2022-04-17 19:52:17 +0300

    Add files via upload

---
+++ form/user.py
@@ -0,0 +1,16 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField
+import sqlalchemy
+from wtforms.validators import DataRequired
+from wtforms import PasswordField, SubmitField
+
+class RegisterForm(FlaskForm):
+    name = StringField('Имя пользователя', validators=[DataRequired()])
+    password = PasswordField('Пароль', validators=[DataRequired()])
+    submit = SubmitField('Войти')
+
+
+class LoginForm(FlaskForm):
+    name = StringField('Имя пользователя', validators=[DataRequired()])
+    password = PasswordField('Пароль', validators=[DataRequired()])
+    submit = SubmitField('Войти')
